534abe4c836216746a2bd36b090d9ee7539710d1
Shivamshukla <114076309+shivamshukla001@users.noreply.github.com>
2023-09-13 13:25:51 +0000

    conversion of datatypes

--- basicsOFjs/basic_1.js
+++ basicsOFjs/basic_1.js
@@ -1,2 +1,32 @@
 
-console.log("shivam")
+let score="33abs"
+
+//console.log(typeof score)
+
+let valueINnumber=Number(score)
+//console.log(valueINnumber);
+
+let bool=1
+
+let boolean=Boolean(bool)
+//console.log(boolean);
+
+
+let bool2 ="shivam"
+//console.log(bool2)
+let boolean2=Boolean(bool2)
+//console.log(boolean2)
+/*
+1=> true
+0 =>falseee
+""=> false
+"shivam"=>true
+
+
+*/
+
+let someNumber=33
+console.log(someNumber);
+
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber);
