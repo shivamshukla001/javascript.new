75db2f57c2e582e80c30dcd98eed5eb9d534037b
Shivamshukla <114076309+shivamshukla001@users.noreply.github.com>
2023-09-14 14:38:44 +0000

    number and math in java

---
+++ basicsOFjs/numsAndMath.js
@@ -0,0 +1,16 @@
+
+//numbers
+
+const hundres=1000000
+console.log(hundres.toLocaleString('en-IN'));//indian standard to calculation
+
+//math
+console.log(Math);
+
+console.log(Math.abs(-9));
+console.log(Math.round(3.3));
+
+console.log(Math.sqrt(99));
+console.log(Math.pow(2,3));
+console.log(Math.max(2,3,4,5));
+console.log(Math.min(2,3,4,5));
