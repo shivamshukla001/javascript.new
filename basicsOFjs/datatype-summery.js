b65131468fb366f34c6fe3fb8e59a010fdbe9efb
Shivamshukla <114076309+shivamshukla001@users.noreply.github.com>
2023-09-14 05:11:27 +0000

    datatype-summery

---
+++ basicsOFjs/datatype-summery.js
@@ -0,0 +1,50 @@
+/**
+ * 2 types of data
+ * first is primitive datatype
+ * 
+ * primitive datatype is 7 type
+ * number,string,boolean,bigInt,symbol,null,undefined
+ * 
+ * example of symbol:-
+ * const id =symbol('123')
+ * const anotherID=symbol('123')
+ * 
+ * 
+ * secons is non primitive datatype
+ * 
+ * non-primitiva datatype is 3 type
+ * 
+ *  array, object,function
+ * 
+ * array:-
+ * 
+ * const hero=["shaktiman","doga","bhulchata"]
+ * 
+ * object:-
+ * 
+ * let obj={
+ * name: "shivam",
+ * age: 22,
+ * address:c-14
+ * }
+ * 
+ * function:-
+ * 
+ * let myFunction=function(){
+ * console.log("hello shivam")
+ * }
+ * myfunction()
+ * 
+ */
+
+let myobj={
+    name: "shivam",
+     age: 22,
+     address:  "c-14"
+    }
+    console.log(myobj);
+
+    let myFunction=function(){
+        console.log("hello shivam")
+     }
+     myFunction();
