4290a1ece552abdcb47f17a4df6fecc88feb7090
Shivamshukla <114076309+shivamshukla001@users.noreply.github.com>
2023-09-14 04:35:09 +0000

    comparison

---
+++ basicsOFjs/conversion.js
@@ -0,0 +1,40 @@
+// console.log("2"+ 2);
+// console.log(2 + "2"+ 2);
+// console.log("2"+ "3"+ 4);
+
+
+let gamecounter=100
+console.log(gamecounter);
+++gamecounter
+console.log(gamecounter);
+gamecounter++
+console.log(gamecounter);
+
+console.log(2>1);
+
+
+console.log(null >0);
+console.log(null>=0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+
+console.log("2"=== 2);
+
+/**
+ * 
+console.log(null >0);
+console.log(null>=0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+console.log("2"=== 2);
+
+
+aisa conversion humesa problem ma daalte hai kyoki javascript ma "==" and ">,<,<=,>=" different work krte hai
+
+line 26 wala consle.log ma false aayga kyoki vha "=" operatoe ka karan null ko "0" sa chota mana hai
+ *  line 27 ma null ke value "true" aayge kyoki ">=" na null ko "0" ka baarabr btaya hai
+
+line 31 ma "===" operator lagaya hai jiska karan value ka datatype bhi check hoga iske liya iske value "false aaygi"
+ */
