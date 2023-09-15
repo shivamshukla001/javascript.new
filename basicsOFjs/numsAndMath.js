9bd81b36d4636595e30a907f2a55f83357e2f53a
Shivamshukla <114076309+shivamshukla001@users.noreply.github.com>
2023-09-15 13:30:27 +0000

    nums_and_math

--- basicsOFjs/numsAndMath.js
+++ basicsOFjs/numsAndMath.js
@@ -1,16 +1,45 @@
 
 //numbers
 
-const hundres=1000000
-console.log(hundres.toLocaleString('en-IN'));//indian standard to calculation
+// const hundres=1000000
+// console.log(hundres.toLocaleString('en-IN'));//indian standard to calculation
 
-//math
-console.log(Math);
+// const balence=new Number(4000000)
+// console.log(balence);
 
-console.log(Math.abs(-9));
-console.log(Math.round(3.3));
+// console.log(balence.toString().length);
 
-console.log(Math.sqrt(99));
-console.log(Math.pow(2,3));
-console.log(Math.max(2,3,4,5));
-console.log(Math.min(2,3,4,5));
+// const otherbalence=1124.9879
+
+// console.log(otherbalence.toPrecision(5));
+// console.log(otherbalence.toFixed(2));
+
+// //math
+// console.log(Math);
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(3.3));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.sqrt(99));
+// console.log(Math.pow(2,3));
+// console.log(Math.max(2,3,4,5));
+// console.log(Math.min(2,3,4,5));
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10)+1);
+
+const min=3;
+const max=20
+
+// console.log(Math.floor((Math.random())* (max-min+1)+min));////  imp formula
+/**
+ * Math.floor iss liya use ho rha hai taaki value ko round off krke accurate la sake 
+ * max-min+1 sa apna ya confirm kr rahe hai ke point sa pahle "0"nai a sakta it means apne pass ans ma kabhi 
+ *  "0"nai aayga
+ * last ma "min" value plus krne sa apna pass value humesa "min" sa bdi aayga, agar apan yha "min=3" rakhte toh 
+ *  apne pass humesa 3 sa bdi value he aati ans ma
+ */
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
