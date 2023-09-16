20a3c8a60818f8cedd09f262c3bb649b4aa595fa
Shivamshukla <114076309+shivamshukla001@users.noreply.github.com>
2023-09-16 15:42:55 +0000

    objects in js

---
+++ basics_02js/object.js
@@ -0,0 +1,30 @@
+
+const mysymbol=Symbol('key1')
+const jsUser={
+    name:'shivam',
+    [mysymbol]: 'key1',
+    email:"shivam@gmail.com",
+    password: 12345,
+    location: "bikaner",
+    LastLoggedIn: false
+}
+
+ jsUser.email="ShivamShukla93";
+ console.log(jsUser.email);
+ 
+ Object.freeze(jsUser)
+
+ console.log(jsUser.email);
+// console.log(jsUser.name);
+// console.log(jsUser.email);
+
+
+
+// console.log(jsUser["email"]);
+// console.log(jsUser["name"]);
+// console.log(jsUser[mysymbol]);
+
+
+
+
+console.log(jsUser);
